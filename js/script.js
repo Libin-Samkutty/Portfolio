38b3a6848dd98c83a99cbca9e633bdf4aa8cf11e
Libin-Samkutty <samlibin2001@gmail.com>
2025-01-19 18:58:05 +0530

    fixed mobile responsive issue in skills page and navbar in contacts page, updated footer section for all pages

--- js/script.js
+++ js/script.js
@@ -15,3 +15,13 @@ window.addEventListener('resize', () => {
         document.body.classList.remove('resize-animation-stopper');
     }, 400);
 });
+
+function sendEmail() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    const mailtoLink = `mailto:samlibin2001@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(`Hey there, Libin! I am ${name}.\n${message}`)}`;
+    window.location.href = mailtoLink;
+}
